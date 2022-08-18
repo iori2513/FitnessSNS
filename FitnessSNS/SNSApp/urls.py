ba2c0eb6e4a78190cd0216af6d5c7394e11e7443
iori <yzhongtian194@gmail.com>
2022-08-18 15:32:41 +0900

    [modify] authenticate機能の細かい修正

--- FitnessSNS/SNSApp/urls.py
+++ FitnessSNS/SNSApp/urls.py
@@ -5,6 +5,4 @@ from . import views
 app_name = 'SNSApp'
 urlpatterns = [
     path('index/', views.IndexView.as_view(), name="index"),
-    path('signup/', views.SignUpView.as_view(), name="signup"),
-    path('signin/', views.SignInView.as_view(), name="signin"),
 ]
